7691c5af75be22c7603ae7f9217c4ba58838c03b
Jeremy.Gu <isdance2004@hotmail.com>
2020-10-29 06:46:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to Kafka Github Issues Connect connector!
+
+
---
+++ src/main/java/org/jeremy/kafka/GitHubSourceConnector.java
@@ -0,0 +1,57 @@
+package org.jeremy.kafka;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GitHubSourceConnector extends SourceConnector {
+    private static Logger log = LoggerFactory.getLogger(GitHubSourceConnector.class);
+    private GitHubSourceConnectorConfig config;
+
+    @Override
+    public String version() {
+        return VersionUtil.getVersion();
+    }
+
+    @Override
+    public void start(Map<String, String> map) {
+        // map will be map of your config parameters
+        // will read from GitHubSourceConnectorExample.properties
+        // because GitHubSourceConnectorExample.properties has connector.class=org.jeremy.kafka.GitHubSourceConnector
+        config = new GitHubSourceConnectorConfig(map);
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return GitHubSourceTask.class;
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int i) {
+        // i is the max.tasks
+        // here we only allow 1.
+        // Define the individual task configurations that will be executed.
+        ArrayList<Map<String, String>> configs = new ArrayList<>(1);
+        configs.add(config.originalsStrings());
+
+        // basically we add 1 copy of config into configs, because we only allow 1 task atm
+        return configs;
+    }
+
+    @Override
+    public void stop() {
+        // Do things that are necessary to stop your connector.
+        // nothing is necessary to stop for this connector
+    }
+
+    @Override
+    public ConfigDef config() {
+        return GitHubSourceConnectorConfig.conf();
+    }
+}
